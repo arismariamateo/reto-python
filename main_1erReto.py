ff3fbfab25b3226d76057a27e8cc1acc60e40b26
arismariamateo <85373845+arismariamateo@users.noreply.github.com>
2024-02-09 11:13:22 -0400

    reto 1 pyton

---
+++ main_1erReto.py
@@ -0,0 +1,10 @@
+#Primer reto de la semana, tu objetivo será poder crear un programa en Python 
+#el cual permita registrar a un usuario en el sistema.
+
+
+nombres   = input('Ingrese Nombre completo: ')
+apellidos = input('Ingrese Apellido completo: ')
+telefono   = int(input('Ingrese su Número de teléfono: '))
+correo = input('Ingrese su correo electrónico: ')
+
+print('Hola ' + nombres +' '+apellidos+',  en breve recibirás un correo a '+ correo)
